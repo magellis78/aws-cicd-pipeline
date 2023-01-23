521f2e234d7209d01749d7bfcda1ef5ccd9dc8bc
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2023-01-23 18:33:41 +0530

    updated cicd-pipeline

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -76,35 +76,4 @@ resource "aws_codepipeline" "cicd_pipeline" {
             }
         }
     }
-
-    stage {
-        name ="Plan"
-        action{
-            name = "Build"
-            category = "Build"
-            provider = "CodeBuild"
-            version = "1"
-            owner = "AWS"
-            input_artifacts = ["tf-code"]
-            configuration = {
-                ProjectName = "tf-cicd"
-            }
-        }
-    }
-
-    stage {
-        name ="Deploy"
-        action{
-            name = "Deploy"
-            category = "Build"
-            provider = "CodeBuild"
-            version = "1"
-            owner = "AWS"
-            input_artifacts = ["tf-code"]
-            configuration = {
-                ProjectName = "tf-cicd"
-            }
-        }
-    }
-
 }
